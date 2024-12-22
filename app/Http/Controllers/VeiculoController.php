72158571b5a4df4941e1a187ab82c6760deb39bb
Isabel <isabel.paulo.mineiro@gmail.com>
2024-12-22 12:45:36 +0100

    adicao de rotaspara os3 ecrans predidos : geral, detalhe, e adicao. Para seguir o exemplo de estudantes tambem adicionei : delete, edit e update. E ja que tinha feito um seeder, tambem criei uma rota para isso, e terei um botao para ele. dei-me ao trabalho por isso.

--- app/Http/Controllers/VeiculoController.php
+++ app/Http/Controllers/VeiculoController.php
@@ -7,10 +7,98 @@ use App\Models\Veiculo;
 
 class VeiculoController extends Controller
 {
-    public function list()
+    /*     public function list()
     {
         $veiculo = Veiculo::factory()->make();
         dd($veiculo->toArray()); 
-    }
+        } */
+
+        //mudança de list para index. Laravel recomenda index invez de list, por cause das linguas utilizadas. 
+        public function index()
+        {
+            $veiculos = Veiculo::all();
+            return view('veiculos.index', compact('veiculos'));
+        }
+
+        //criar um veiculo
+        public function create(){return view('veiculos.create');}
+
+
+        //Reutilisando estudante
+        public function store(Request $request)
+        {
+            $request->validate([
+                'marca' => 'required',
+                'modelo' => 'required',
+                'matricula' => 'required|unique:veiculos|max:6',
+                'foi_comprado' => 'boolean',
+                'tipo_combustivel' => 'required|string|max:20',
+                //fazer de cor uma picklist (lembrete criar)
+                'cor' => 'required|string|max:20',
+                'ano' => 'nullable|integer',
+                //com laravel é nullable e nao opcional
+                'vezes_considerada_compra' => 'nullable|integer',
+            ]);
+    
+            $marcaToInsert = $request['marca'];
+            $modeloToInsert = $request['modelo'];
+            $matriculaToInsert = $request['matricula'];
+            $foiCompradoToInsert = $request['foi_comprado'] ?? false;
+            $tipoCombustivelToInsert = $request['tipo_combustivel'];
+            $corToInsert = $request['cor'];
+            $anoToInsert = $request['ano'] ?? now()->lastyear; //ano do carro, se nao meterem por default sera do ano passado ? acho que é assim que funcionam.
+            $anoToInsert = $request['ano'];
+            //ver se posso meter 0 por default visto que foi acabado de ser adicionado
+            $vezes_considerada_compraToInsert = $request['vezes_considerada_compra']?? 0 ;
+
+            // Student::create($request->all());
+            Veiculo::create([
+                'marca' => $marcaToInsert,
+                'modelo' => $modeloToInsert,
+                'matricula' => $matriculaToInsert,
+                'foi_comprado' => $foiCompradoToInsert,
+                'tipo_combustivel' => $tipoCombustivelToInsert,
+                'cor' => $corToInsert,
+                'ano' => $anoToInsert,
+                'vezes_considerada_compra' => $vezes_considerada_compraToInsert,
+            ]);
+    
+
+            return redirect()->route('veiculos.index')->with('success', 'Parabens pelo o novo veiculo!');
+         }
+
+         //ja que criei seeders e factories, preferia tambem utilisar ou ao menos propor para o projeto 
+         public function seed()
+         {
+             Veiculo::factory()->count(10)->create();
+             return redirect()->route('veiculos.index')->with('success', '10 veículos criados com sucesso!');
+         }
+
+
+
+        public function edit(Veiculo $veiculo)
+        {
+            return view('veiculos.edit', compact('veiculo'));
+        }
+        public function update(Request $request, Veiculo $veiculo)
+        {
+            $request->validate([
+                'marca' => 'required',
+                'modelo' => 'required',
+                'matricula' => 'required|unique:veiculos,matricula|max:6',
+                'foi_comprado' => 'boolean',
+                'tipo_combustivel' => 'required|string|max:20',
+                'cor' => 'required|string|max:20',
+                'ano' => 'nullable|integer',
+                'vezes_considerada_compra' => 'nullable|integer',
+            ]);
+            $veiculo->update($request->all());
+            return redirect()->route('veiculos.index')->with('success', 'Veiculo atualizado');
+        }
+        public function destroy(Veiculo $veiculo)
+        {
+            $veiculo->delete();
+            return redirect()->route('veiculos.index')->with('success', 'Veiculo apagado da sua conta');
+        }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,38 @@ use App\Http\Controllers\VeiculoController;
 use App\Http\Controllers\EmpregadoController;
 use App\Http\Controllers\VeiculoEmpregadoController;
 
+//Primeiras rotas para testar banco de dados
+
 Route::get('/', function () {
-    return "Primeira rota criada para a EmpresaFofinhos";
+    return "Pagina Inicial : Primeira rota criada para a EmpresaFofinhos";
 });
 
 Route::get('/lista-veiculos-empregados', [VeiculoEmpregadoController::class, 'list']);
 
-Route::get('/lista-veiculos', [VeiculoController::class, 'list']);
 Route::get('/lista-empregados', [EmpregadoController::class, 'list']);
+
+
+// Rotas de visualisacao da informacao geral sobre a frota,
+Route::get('/veiculos', [VeiculoController::class, 'index'])->name('veiculos.index');
+
+
+//Ecra de formulario
+Route::get('/veiculos/create', [VeiculoController::class, 'create'])->name('veiculos.create');
+// como em management students, adicionou um metodo para gravar o veiculo criado:
+Route::post('/veiculos', [VeiculoController::class, 'store'])->name('veiculos.store');
+//parar criar 10 automaticamente :
+Route::get('/seed-veiculos', [VeiculoController::class, 'seed'])->name('veiculos.seed');
+
+//editar o veiculo
+Route::get('/veiculos/{veiculo}/edit', [VeiculoController::class, 'edit'])->name('veiculos.edit');
+Route::put('/veiculos/{veiculo}', [VeiculoController::class, 'update'])->name('veiculos.update');
+
+// apagar o veiculo
+Route::delete('/veiculos/{veiculo}', [VeiculoController::class, 'destroy'])->name('veiculos.destroy');
+
+
+//Detalhes do veiculo
+Route::get('/veiculos/{id}', [VeiculoController::class, 'show'])->name('veiculos.show');
 
 
 
